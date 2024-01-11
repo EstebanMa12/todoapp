6edffacf150033b2c96d3d8a7d69ae011eb38c26
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-11 11:13:38 -0500

    Add Angular TODO app to home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass'
 })
 export class HomeComponent {
+  tasks = signal([
+    'Instalar Angular CLI',
+    'Crear proyecto',
+    'Crear componentes',
+    'Crear servicios',
+    'Crear rutas',
+    'Crear modelos',
+    'Crear interfaces',
+    'Crear pipes',
+    'Crear directivas',
+    'Crear guards',
+  ])
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newTask = input.value;
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+  deleteTask(index:number){
+    this.tasks.update((tasks) => tasks.filter((task, i) => i !== index));
+  }
 
 }
