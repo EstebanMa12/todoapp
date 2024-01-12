32eb371200b345e8356bfd5e24e67edcfb80b3c0
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-12 11:56:31 -0500

    Add task editing functionality

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,49 +1,67 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Task } from '../../models/task.model';
-import { log } from 'console';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.sass'
+  styleUrl: './home.component.sass',
 })
 export class HomeComponent {
   tasks = signal<Task[]>([]);
-  taskIdCounter = this.tasks.length+1;
+  taskIdCounter = this.tasks.length + 1;
   changeHandler(event: Event) {
     const input = event.target as HTMLInputElement;
     const newTask = input.value;
     this.addTask(newTask);
     input.value = '';
   }
-  addTask(title: string){
+  addTask(title: string) {
     const newTask = {
       id: this.taskIdCounter,
       title,
       completed: false,
+      edited:false,
     };
     this.tasks.update((tasks) => [...tasks, newTask]);
     this.taskIdCounter++;
   }
-  deleteTask(index:number){
+  deleteTask(index: number) {
     this.tasks.update((tasks) => tasks.filter((_, i) => i !== index));
   }
-  completedTask(index: number){
+  completedTask(index: number) {
     this.tasks.update((tasks) =>
-    tasks.map((task, position) => {
-      if (position === index) {
-
-        return {
-          ...task,
-          completed: !task.completed,
-        };
-      }
-      return task;
-    })
-  );
+      tasks.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            completed: !task.completed,
+          };
+        }
+        return task;
+      })
+    );
   }
-
+  // updateTask(index: number, event: Event)
+  updateTask(index: number){
+    this.tasks.update((tasks) =>
+      tasks.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            edited: !task.edited,
+          };
+        }
+        return task;
+      })
+    );
+  }
+  changeHandlerUpdate(event: Event){
+    const input = event.target as HTMLInputElement;
+    const newTask = input.value;
+    this.addTask(newTask);
+    input.value = '';
+  }
 }
